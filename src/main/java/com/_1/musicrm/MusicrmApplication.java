d301b10b4e2a2f535117b1f40850cc707f7c2509
hong21 <hhhong212121@gmail.com>
2025-03-20 03:05:32 +0800

    new spring boot project files created

---
+++ src/main/java/com/_1/musicrm/MusicrmApplication.java
@@ -0,0 +1,13 @@
+package com._1.musicrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_1/musicrm/MusicrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com._1.musicrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
